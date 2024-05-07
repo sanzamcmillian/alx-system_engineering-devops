eccfe0c1ac80bdd87b15111ddc0994e528dbc2c3
sanzamcmillian <g18s2658@ru.ac.za>
2024-05-07 16:54:57 +0200

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,5 @@ def number_of_subscribers(subreddit):
         subscribers = data['data']['subscribers']
         return subscribers
 
-    elif response.status_code == 404:
-        return 0
-
     else:
-        print(f"Error: {response.status_code} - {response.reason}")
         return 0
